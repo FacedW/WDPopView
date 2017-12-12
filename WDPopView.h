7c19363955f4d33c113fc9275f5aeda74d4125d6
Faced Wu <15209200621@163.com>
2017-12-12 18:25:15 +0800

    init file

---
+++ WDPopView.h
@@ -0,0 +1,75 @@
+//
+//  WDPopView.h
+//  TNDataDisplay
+//
+//  Created by 吴鼎 on 2017/12/11.
+//  Copyright © 2017年 TN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger,WDPopStyle) {
+    WDPopStyleFromBottom = 0,
+    WDPopStyleFromTop,
+};
+
+@protocol WDPopViewDelegate;
+@interface WDPopView : NSObject
+/**
+ 弹出方式，默认WDPopStyleFromBottom
+ */
+@property (nonatomic,assign) WDPopStyle popStyle;
+/**
+ 弹窗背景色,默认0.6灰度
+ */
+@property (nonatomic,strong) UIColor *popViewBackgroundColor;
+/**
+ 是否需要遮罩，默认YES
+ */
+@property (nonatomic,assign) BOOL isMasked;
+/**
+ 代理
+ */
+@property (nonatomic,assign) id <WDPopViewDelegate>delegate;
+
+
+
+/**
+ 初始化
+
+ @param contentView 自定义的view
+ @return popview
+ */
+- (instancetype)initWithContentView:(UIView *)contentView;
+/**
+ 弹窗弹出
+
+ @param animated 是否需要动画
+ */
+- (void)presentPopupControllerAnimate:(BOOL)animated;
+/**
+ 弹窗收回
+ 
+ @param animated 是否需要动画
+ */
+- (void)dismissPopupControllerAnimated:(BOOL)animated;
+@end
+
+
+
+
+@protocol WDPopViewDelegate <NSObject>
+@optional
+/**
+ 已经弹出
+ 
+ @param contentView contentView
+ */
+- (void)WDPopContentViewDidPresent:(UIView*)contentView;
+/**
+ 已经退出
+ 
+ @param contentView contentView
+ */
+- (void)WDPopContentViewDidDismiss:(UIView*)contentView;
+@end
---
+++ WDPopView.m
@@ -0,0 +1,152 @@
+//
+//  WDPopView.m
+//  TNDataDisplay
+//
+//  Created by 吴鼎 on 2017/12/11.
+//  Copyright © 2017年 TN. All rights reserved.
+//
+
+#import "WDPopView.h"
+@interface WDPopView()<UIGestureRecognizerDelegate>
+@property (nonatomic,strong) UIView *popBackgroundView;
+@property (nonatomic,strong) UIView *contentView;
+
+@property (nonatomic,assign) CGPoint presentCenter;
+@property (nonatomic,assign) CGPoint afterCenter;
+@end
+@implementation WDPopView
+#define WDPopScreenBounds [UIScreen mainScreen].bounds
+#define WDPopStatusBarHeight [[UIApplication sharedApplication] statusBarFrame].size.height
+#define WDPopNavBarHeight 44.0
+
+- (instancetype)initWithContentView:(UIView *)contentView{
+    self = [super init];
+    if (self) {
+        self.contentView = contentView;
+        [self completeDefaultPropsSet];
+        [self setupUI];
+    }
+    return self;
+}
+
+//完成属性默认值设置
+- (void)completeDefaultPropsSet{
+    self.isMasked = YES;
+    self.popStyle = WDPopStyleFromBottom;
+    self.popViewBackgroundColor = [UIColor colorWithWhite:0 alpha:0.6];
+}
+//UI设置
+- (void)setupUI{
+    [self.popBackgroundView addSubview:self.contentView];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(backgroundViewTap)];
+    tap.delegate = self;
+    [self.popBackgroundView addGestureRecognizer:tap];
+}
+#pragma mark - popView的弹出和收回
+- (void)presentPopupControllerAnimate:(BOOL)animated{
+    [self setBackgroundViewFrame];
+    [self setContentViewFrame];
+    UIView *mainView = [UIApplication sharedApplication].keyWindow.rootViewController.view;
+    [mainView addSubview:self.popBackgroundView];
+    if (animated) {
+        __weak typeof(self) weakself = self;
+        [UIView animateWithDuration:0.3 animations:^{
+            weakself.contentView.center = weakself.afterCenter;
+            if (weakself.isMasked) {
+                weakself.popBackgroundView.backgroundColor = weakself.popViewBackgroundColor;
+            }
+        }];
+    } else {
+        self.contentView.center = self.afterCenter;
+        if (self.isMasked) {
+            self.popBackgroundView.backgroundColor = self.popViewBackgroundColor;
+        }
+    }
+    if ([self.delegate respondsToSelector:@selector(WDPopContentViewDidPresent:)]) {
+        [self.delegate WDPopContentViewDidPresent:self.contentView];
+    }
+}
+
+- (void)dismissPopupControllerAnimated:(BOOL)animated{
+    if (animated) {
+        __weak typeof(self) weakself = self;
+        [UIView animateWithDuration:0.3 animations:^{
+            weakself.contentView.center = weakself.presentCenter;
+            weakself.popBackgroundView.backgroundColor = [UIColor clearColor];
+        } completion:^(BOOL finished) {
+            [weakself.popBackgroundView removeFromSuperview];
+        }];
+    } else {
+        self.contentView.center = self.presentCenter;
+        self.popBackgroundView.backgroundColor = [UIColor clearColor];
+        [self.popBackgroundView removeFromSuperview];
+    }
+    if ([self.delegate respondsToSelector:@selector(WDPopContentViewDidDismiss:)]) {
+        [self.delegate WDPopContentViewDidDismiss:self.contentView];
+    }
+}
+
+#pragma mark - 背景手势
+- (void)backgroundViewTap{
+    [self dismissPopupControllerAnimated:YES];
+}
+
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer{
+    if ([gestureRecognizer locationInView:self.popBackgroundView].y>=self.contentView.origin.y && self.popStyle==WDPopStyleFromBottom) {
+        return NO;
+    }
+    if ([gestureRecognizer locationInView:self.popBackgroundView].y<=CGRectGetMaxY(self.contentView.frame) && self.popStyle == WDPopStyleFromTop) {
+        return NO;
+    }
+    if ([self.delegate respondsToSelector:@selector(WDPopContentViewDidDismiss:)]) {
+        [self.delegate WDPopContentViewDidDismiss:self.contentView];
+    }
+    return YES;
+}
+#pragma mark - frame设置
+- (void)setBackgroundViewFrame{
+    if (self.isMasked) {return;}
+    switch (self.popStyle) {
+        case WDPopStyleFromTop:
+            self.popBackgroundView.frame = CGRectMake(self.popBackgroundView.frame.origin.x, self.popBackgroundView.frame.origin.y, self.popBackgroundView.frame.size.width, self.contentView.frame.size.height);
+            break;
+        case WDPopStyleFromBottom:
+            self.popBackgroundView.frame = CGRectMake(self.popBackgroundView.frame.origin.x, self.popBackgroundView.frame.size.height-self.contentView.frame.size.height, self.popBackgroundView.frame.size.width, self.contentView.frame.size.height);
+            break;
+        default:
+            break;
+    }
+}
+
+
+- (void)setContentViewFrame{
+    switch (self.popStyle) {
+        case WDPopStyleFromTop:
+            self.presentCenter = CGPointMake(self.popBackgroundView.size.width/2, -self.contentView.size.height/2);
+            self.afterCenter = CGPointMake(self.popBackgroundView.size.width/2, self.contentView.size.height/2);
+            break;
+        case WDPopStyleFromBottom:
+            self.presentCenter = CGPointMake(self.popBackgroundView.size.width/2, self.popBackgroundView.size.height+self.contentView.size.height/2);
+            self.afterCenter = CGPointMake(self.popBackgroundView.size.width/2, self.popBackgroundView.size.height-self.contentView.size.height/2);
+            break;
+        default:
+            break;
+    }
+    self.contentView.center = self.presentCenter;
+}
+#pragma mark - getter和setter
+- (UIView *)popBackgroundView{
+    if (!_popBackgroundView) {
+        _popBackgroundView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, WDPopScreenBounds.size.width, WDPopScreenBounds.size.height)];
+        _popBackgroundView.layer.masksToBounds = YES;
+    }
+    return _popBackgroundView;
+}
+
+- (void)setPopStyle:(WDPopStyle)popStyle{
+    _popStyle = popStyle;
+    if (popStyle == WDPopStyleFromTop) {
+        self.popBackgroundView.frame = CGRectMake(0, WDPopStatusBarHeight+WDPopNavBarHeight, WDPopScreenBounds.size.width, WDPopScreenBounds.size.height-WDPopStatusBarHeight-WDPopNavBarHeight);
+    }
+}
+@end
